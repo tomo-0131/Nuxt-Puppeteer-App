ff31a7e95c508e3a150fa2897511c5f0938d7bc7
ecru <woovoodoor@yahoo.co.jp>
2020-12-11 16:58:39 +0900

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header">
+    <div class="header-inner">
+      <h1 class="title">FARFETCH SCRAPING !</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.header {
+  top: 0;
+  position: sticy;
+}
+
+.title {
+  margin-top: 20px;
+  text-align: center;
+}
+
+</style>
