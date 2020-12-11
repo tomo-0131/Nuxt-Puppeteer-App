19612e14c066f25ff917974f6fe5db88cf647709
ecru <woovoodoor@yahoo.co.jp>
2020-12-11 16:58:43 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-main>
+      <Header />
+        <v-container>
+          <nuxt />
+        </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Header from '~/components/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
