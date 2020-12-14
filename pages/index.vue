2a1590be865ef64b6962d71f0fda95bf3c4896b7
ecru <woovoodoor@yahoo.co.jp>
2020-12-14 16:01:01 +0900

    一部コンポーネント化

--- pages/index.vue
+++ pages/index.vue
@@ -13,39 +13,9 @@
         <v-card-title class="headline">
           <h5 class="scraping-head">S C R A P I I N G</h5>
         </v-card-title>
-          <div id="tabs">
-            <v-card color="basil">
-              <v-tabs
-                v-model="tab"
-                background-color="transparent"
-                color="info"
-                grow
-              >
-                <v-tab
-                  v-for="item in items"
-                   max-width="200px"
-                  :key="item.tab"
-                >
-                  {{ item.tab }}
-                </v-tab>
-              </v-tabs>
-            <v-row justify="center" align="center">
-              <v-tabs-items v-model="tab">
-                <v-tab-item
-                  v-for="item in items"
-                  :key="item.tab"
-                >
-                  <v-card
-                    flat
-                  >
-                    <v-card-text>{{ item.content }}</v-card-text>
-                  </v-card>
-                </v-tab-item>
-              </v-tabs-items>
-            </v-row>
-            </v-card>
-          </div>
-        </div>
+      </div>
+      <div>
+        <TabContents />
         <br>
         <v-card-actions>
           <v-row justify="center" align="center">
@@ -57,31 +27,16 @@
               nuxt
               to="/scraping"
             >
-
               SCRAPING!
-
             </v-btn>
-
           </v-row>
         </v-card-actions>
+        <SoContentsTable />
+      </div>
       </v-card>
     </v-col>
   </v-row>
 </template>
-
-<script>
-  export default {
-    data(){
-      return {
-        tab: null,
-        items: [
-          { tab: 'What is Scraping?', content: '”Scraping”” means collecting data and processing it so that it is easy to use.' },
-          { tab: 'Tech Stack', content: 'Nuxt.js, Vuetify, Puppeteer' },
-        ],
-      }
-    },
-  }
-</script>
 
 <style>
 .scraping-head  {
