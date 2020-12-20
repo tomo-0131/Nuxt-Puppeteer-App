0a7f5fa6d0a8f42c5398a94b1ad4ebdbd08de4d1
ecru <woovoodoor@yahoo.co.jp>
2020-12-21 08:54:40 +0900

    レイアウト変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,42 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="9" md="6">
-      <v-card
-        elevation="2"
-        height="470px"
-      >
+      <v-card elevation="2" height="470px">
         <v-img
           src="https://res.fashionsnap.com/image/upload/f_auto,q_auto/asset/collection/images/2020/08/sacai-21ss-pre-001.jpg?1607036664240"
           height="200px"
         ></v-img>
-      <div>
-        <v-card-title class="headline">
-          <h5 class="scraping-head">S C R A P I I N G</h5>
-        </v-card-title>
-      </div>
-      <div>
-        <TabContents />
-        <br>
-        <v-card-actions>
-          <v-row justify="center" align="center">
-            <v-btn
-              class="v-btn-scraping"
-              outlined
-              color="Yellow"
-              elevation="5"
-              nuxt
-              to="/scraping"
-            >
-              SCRAPING!
-            </v-btn>
-          </v-row>
-        </v-card-actions>
-        <SoContentsTable />
-      </div>
+        <div>
+          <v-card-title class="headline">
+            <h5 class="scraping-head">S C R A P I I N G</h5>
+          </v-card-title>
+        </div>
+        <div>
+          <TabContents />
+          <br />
+          <v-card-actions>
+            <v-row justify="center" align="center">
+              <v-btn
+                class="v-btn-scraping"
+                outlined
+                color="Yellow"
+                elevation="5"
+                nuxt
+                to="/api/users"
+              >
+                SCRAPING!
+              </v-btn>
+            </v-row>
+          </v-card-actions>
+          <SoContentsTable />
+        </div>
       </v-card>
     </v-col>
   </v-row>
 </template>
 
 <style>
-.scraping-head  {
+.scraping-head {
   margin: 0 auto;
 }
 
