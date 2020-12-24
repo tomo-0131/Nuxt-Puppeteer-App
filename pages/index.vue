f3e1c78568b4519e7eac6b6784dcfa1a52497357
ecru <woovoodoor@yahoo.co.jp>
2020-12-24 09:53:42 +0900

    修正

--- pages/index.vue
+++ pages/index.vue
@@ -24,7 +24,7 @@
 <script>
 export default {
   async asyncData({ $axios }) {
-    const items = await $axios.$get("https://localhost:5000/");
+    const items = await $axios.$get("http://localhost:5000/");
     return { items };
   },
 };
