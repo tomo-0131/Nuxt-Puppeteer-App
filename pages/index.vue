beef6e983ba6498ff4e4d470d331c3217dbc528e
ecru <woovoodoor@yahoo.co.jp>
2020-12-27 00:34:34 +0900

    レイアウト変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,34 @@
 <template>
-  <v-card class="mx-auto" max-width="500" max-height="500">
-    <br />
-    <v-list-item three-line>
-      <div class="scraping">
-        <h5>ID：{{ items.id }}</h5>
-        <br />
-        <h5>商品画像URL：{{ items.imageUrl }}</h5>
-        <br />
-        <h5>ブランド名：{{ items.brandName }}</h5>
-        <br />
-        <h5>アイテム名：{{ items.itemName }}</h5>
-        <br />
-        <h5>価格：￥{{ items.price }} (税込)</h5>
-        <br />
-        <h5>素材：{{ items.material }}</h5>
-        <br />
-        <h5>ブランドスタイルID：{{ items.brandStyleId }}</h5>
-      </div>
-    </v-list-item>
-    <br />
-  </v-card>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="9" md="6">
+      <v-card elevation="2" height="610px">
+        <v-img
+          src="https://res.fashionsnap.com/image/upload/f_auto,q_auto/asset/collection/images/2020/08/sacai-21ss-pre-001.jpg?1607036664240"
+          height="180px"
+        ></v-img>
+        <TabContents />
+        <div class="container">
+          <div>
+            <div class="main">
+              <h5>ID：{{ items.id }}</h5>
+              <br />
+              <h5>商品画像URL：{{ items.imageUrl }}</h5>
+              <br />
+              <h5>ブランド名：{{ items.brandName }}</h5>
+              <br />
+              <h5>アイテム名：{{ items.itemName }}</h5>
+              <br />
+              <h5>価格：￥{{ items.price }} (税込)</h5>
+              <br />
+              <h5>素材：{{ items.material }}</h5>
+              <br />
+              <h5>ブランドスタイルID：{{ items.brandStyleId }}</h5>
+            </div>
+          </div>
+        </div>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
