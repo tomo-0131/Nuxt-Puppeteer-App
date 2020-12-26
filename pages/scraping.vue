cd59a7e76c3cd21bff8f08c64b8ed7aae04e6124
ecru <woovoodoor@yahoo.co.jp>
2020-12-27 00:34:40 +0900

    レイアウト変更

--- pages/scraping.vue
+++ pages/scraping.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        FARFETCH PUPPETEER
-      </h1>
       <div class="main">
         <h2>アイテム詳細</h2>
         <p class="time">更新日時：{{ data.datetime }}</p>
@@ -16,7 +12,6 @@
           </thead>
           <tbody>
             <td>{{ brand.name }}</td>
-
           </tbody>
         </table>
       </div>
@@ -24,30 +19,27 @@
   </div>
 </template>
 
-
 <script>
 export default {
   async asyncData({ $axios }) {
-    const data = await $axios.$get('/api/scraping')
-    return { data }
+    const data = await $axios.$get("/api/scraping");
+    return { data };
   },
 
   data: () => ({
-    data: []
+    data: [],
   }),
 
   methods: {
     reload() {
-      location.reload()
-    }
-  }
-}
+      location.reload();
+    },
+  },
+};
 </script>
 
 <style>
-
 .article-top {
   text-align: center;
 }
-
 </style>
