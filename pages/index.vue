83aa3f1cc678127503d2293f31229b17b7d4aec3
ecru <woovoodoor@yahoo.co.jp>
2020-12-31 10:19:25 +0900

    TOP 画像サイズ修正

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <v-col cols="12" sm="9" md="6">
       <v-img
         src="https://res.fashionsnap.com/image/upload/f_auto,q_auto/asset/collection/images/2020/08/sacai-21ss-pre-001.jpg?1607036664240"
-        height="180px"
+        height="250px"
       ></v-img>
       <br />
       <v-divider></v-divider>
