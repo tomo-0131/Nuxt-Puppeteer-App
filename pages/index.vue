bda79b753ad454d241de428fcd46b6ee23c8c115
ecru <woovoodoor@yahoo.co.jp>
2021-01-03 16:24:12 +0900

    フロント修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,33 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="9" md="6">
+    <v-col cols="10">
       <v-img
         src="https://res.fashionsnap.com/image/upload/f_auto,q_auto/asset/collection/images/2020/08/sacai-21ss-pre-001.jpg?1607036664240"
-        height="250px"
+        height="210px"
       ></v-img>
       <br />
       <v-divider></v-divider>
+      <v-card
+        class="text-center"
+        color="light-green darken-1"
+        dark
+        elevation="3"
+      >
+        <h3>SACAI</h3>
+      </v-card>
+      <br />
+      <v-container class="grey lighten-5">
+        <v-row
+          v-for="n in 2"
+          :key="n"
+          :class="n === 1 ? 'mb-6' : ''"
+          no-gutters
+        >
+          <v-col v-for="k in n + 1" :key="k">
+            <v-card class="pa-2" outlined tile> {{ k }} of {{ n + 1 }} </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
       <br />
       <TabContents />
       <div class="container"></div>
@@ -29,10 +50,10 @@
 </template>
 
 <script>
-export default {
-  async asyncData({ $axios }) {
-    const items = await $axios.$get("http://localhost:5000/api");
-    return { items };
-  },
-};
+//export default {
+//async asyncData({ $axios }) {
+//const items = await $axios.$get("http://localhost:5000/");
+//return { items };
+//},
+//};
 </script>
