e388deaa5546bcf53914c5941c2aae28d83ef827
ecru <woovoodoor@yahoo.co.jp>
2021-01-04 23:33:42 +0900

    不要文字削除

--- components/TabContents.vue
+++ components/TabContents.vue
@@ -11,7 +11,7 @@
           {{ item.tab }}
         </v-tab>
       </v-tabs>
-      <br>っxっz
+      <br />
       <v-row justify="center" align="center">
         <v-tabs-items v-model="tab">
           <v-tab-item v-for="item in items" :key="item.tab">
