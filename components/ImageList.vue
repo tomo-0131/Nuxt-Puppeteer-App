70b6da74ab85d5f265c0f8c1c9f9e5ce4cbf8d90
ecru <woovoodoor@yahoo.co.jp>
2021-01-05 20:21:49 +0900

    イメージリストの配置修正

--- components/ImageList.vue
+++ components/ImageList.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row>
-    <v-col v-for="n in 9" :key="n" class="d-flex child-flex" cols="4">
+    <v-col v-for="n in 8" :key="n" class="d-flex child-flex" cols="6">
       <v-img
         :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
         :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
