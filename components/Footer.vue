73293d97a1649e6b5858699625afa2c45ab0ac2f
ecru <woovoodoor@yahoo.co.jp>
2021-01-05 23:00:30 +0900

    Footerコンポーネント作成

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,40 +1,10 @@
 <template>
-  <div id="app">
-    <v-app id="inspire">
-      <v-footer dark padless>
-        <v-card flat tile class="indigo lighten-1 white--text text-center">
-          <v-card-text>
-            <v-btn
-              v-for="icon in icons"
-              :key="icon"
-              class="mx-10 white--text"
-              icon
-            >
-              <v-icon size="24px">
-                {{ icon }}
-              </v-icon>
-            </v-btn>
-          </v-card-text>
-
-          <v-card-text class="white--text pt-0">
-            Nuxt.js, Node.js, Puppeteer, Express
-          </v-card-text>
-
-          <v-divider></v-divider>
-
-          <v-card-text class="white--text">
-            {{ new Date().getFullYear() }} — <strong>FARFETCH SCRAPING!</strong>
-          </v-card-text>
-        </v-card>
-      </v-footer>
-    </v-app>
-  </div>
+  <v-footer absolute>
+    <div class="footer">Copyright PLIME 2020. 　All right reserved</div>
+  </v-footer>
 </template>
-
-<script>
-export default {
-  data: () => ({
-    icons: ["mdi-github", "mdi-twitter"],
-  }),
-};
-</script>
+<style lang="scss" scoped>
+.footer {
+  margin: auto;
+}
+</style>
