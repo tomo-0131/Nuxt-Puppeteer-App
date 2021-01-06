9ad00f51840d8861effa5e42bc9f6e18538e868e
ecru <woovoodoor@yahoo.co.jp>
2021-01-06 21:08:24 +0900

    pages(about)の追記

--- pages/About.vue
+++ pages/About.vue
@@ -1,1 +1,47 @@
-<template></template>
+<template>
+  <v-container>
+    <div class="text-center">
+      <h2>A B O U T</h2>
+    </div>
+    <br />
+    <v-row justify="center">
+      <v-img
+        class="justify-center"
+        lazy-src="https://picsum.photos/id/11/10/"
+        max-height="600"
+        max-width="400"
+        src="https://picsum.photos/id/1067/500/300"
+      ></v-img>
+      <br />
+    </v-row>
+    <v-row justify="center">
+      <div style="white-space: pre-wrap; word-wrap: break-word">
+        <br />
+        <h2 class="text-center">P L I M E</h2>
+        PLIMEは世界中のファッションブランドを集めた、ファッション総合ECサイトです。
+        <div class="text-center">
+          全てのファッションユーザー対し、最新のweb技術による無駄のない洗練された
+          ユーザー体験をお届けするため新たなファッションサービスを提供します。
+        </div>
+        <div class="text-center" font-size="10">
+          PLIME is a comprehensive fashion e-commerce site that collects fashion
+          brands from all over the world. We will provide new fashion services
+          to all fashion users in order to provide a lean and sophisticated user
+          experience with the latest web technology.
+        </div>
+      </div>
+    </v-row>
+    <br />
+    <v-row justify="center">
+      <v-img
+        class="justify-center"
+        lazy-src="https://picsum.photos/id/11/10/6"
+        max-height="600"
+        max-width="400"
+        src="https://picsum.photos/id/147/500/300?greyscale"
+      ></v-img>
+      <br />
+    </v-row>
+    <br />
+  </v-container>
+</template>
