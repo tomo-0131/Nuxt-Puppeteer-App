5ce241330317437eaf0b32376f477419dcd08264
ecru <woovoodoor@yahoo.co.jp>
2021-01-06 17:43:42 +0900

    Studionicholson レイアウト修正

--- pages/Studionicholson.vue
+++ pages/Studionicholson.vue
@@ -1,1 +1,37 @@
-<template></template>
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="10">
+      <v-img
+        src="https://res.fashionsnap.com/image/upload/f_auto,q_auto/asset/collection/images/2020/08/sacai-21ss-pre-001.jpg?1607036664240"
+        height="210px"
+      ></v-img>
+      <br />
+      <v-divider></v-divider>
+      <v-card class="text-center" color="light-green" dark elevation="3">
+        <h3>STUDIO NICHOLSON</h3>
+      </v-card>
+      <br />
+      <v-divider></v-divider>
+      <br />
+      <h5 class="grey--text text--darken-2 text-center">
+        Comme des
+        Garçonsで勤めた経歴を持つ阿部千登勢が1999年に立ち上げたブランド。
+      </h5>
+      <br />
+      <v-divider></v-divider>
+      <br />
+      <ImageList />
+      <br />
+      <br />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+//export default {
+//async asyncData({ $axios }) {
+//const items = await $axios.$get("http://localhost:5000/");
+//return { items };
+//},
+//};
+</script>
