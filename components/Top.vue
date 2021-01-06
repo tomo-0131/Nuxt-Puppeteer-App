4b20194bcd135f34bcc170d983f144007e67307a
ecru <woovoodoor@yahoo.co.jp>
2021-01-06 16:11:33 +0900

    Topコンポーネント作成

---
+++ components/Top.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="10">
+      <v-img
+        src="https://res.fashionsnap.com/image/upload/f_auto,q_auto/asset/collection/images/2020/08/sacai-21ss-pre-001.jpg?1607036664240"
+        height="210px"
+      ></v-img>
+      <br />
+      <v-divider></v-divider>
+      <br />
+      <v-divider></v-divider>
+      <br />
+      <ImageList />
+      <br />
+      <v-divider></v-divider>
+      <br />
+      <TabContents />
+      <br />
+    </v-col>
+  </v-row>
+</template>
