2f3e7e59cbc05619163d825882c6349283474a76
ecru <woovoodoor@yahoo.co.jp>
2021-01-06 23:26:31 +0900

    文章レイアウトの修正

--- pages/About.vue
+++ pages/About.vue
@@ -15,15 +15,18 @@
       <br />
     </v-row>
     <v-row justify="center">
-      <div style="white-space: pre-wrap; word-wrap: break-word">
+      <div style="white-space: wrap; word-wrap: break-word">
         <br />
         <h2 class="text-center">P L I M E</h2>
-        PLIMEは世界中のファッションブランドを集めた、ファッション総合ECサイトです。
         <div class="text-center">
-          全てのファッションユーザー対し、最新のweb技術による無駄のない洗練された
+          PLIMEは世界中のファッションブランドを集めた、ファッション総合ECサイトです。
+        </div>
+        <div class="text-center">
+          全てのファッションユーザー対し、
+          最新のweb技術による無駄のない洗練された
           ユーザー体験をお届けするため、革新的なファッションサービスを提供します。
         </div>
-        <div class="text-center" font-size="10">
+        <div class="text-center">
           PLIME is a comprehensive fashion e-commerce site that collects fashion
           brands from all over the world. We will provide new fashion services
           to all fashion users in order to provide a lean and sophisticated user
