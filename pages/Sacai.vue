4fc0a654d4bc4c1e9684ff0da97fec4836988b71
ecru <woovoodoor@yahoo.co.jp>
2021-01-06 17:09:05 +0900

    ディレクトリ変更(pages)

---
+++ pages/Sacai.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="10">
+      <v-img
+        src="https://res.fashionsnap.com/image/upload/f_auto,q_auto/asset/collection/images/2020/08/sacai-21ss-pre-001.jpg?1607036664240"
+        height="210px"
+      ></v-img>
+      <br />
+      <v-divider></v-divider>
+      <v-card
+        class="text-center"
+        color="light-green darken-1"
+        dark
+        elevation="3"
+      >
+        <h3>SACAI</h3>
+      </v-card>
+      <br />
+      <v-divider></v-divider>
+      <br />
+      <h5 class="grey--text text--darken-2 text-center">
+        Comme des
+        Garçonsで勤めた経歴を持つ阿部千登勢が1999年に立ち上げたブランド。
+      </h5>
+      <br />
+      <v-divider></v-divider>
+      <br />
+      <ImageList />
+      <br />
+      <v-divider></v-divider>
+      <br />
+      <TabContents />
+
+      <div class="container"></div>
+      <v-row justify="center" align="center">
+        <v-btn
+          class="ma-1"
+          color="light-green darken-1"
+          dark
+          elevation="5"
+          nuxt
+          to="/api"
+        >
+          <v-icon dark right> mdi-checkbox-marked-circle </v-icon>
+          　SCRAPING!　
+        </v-btn>
+        <br />
+      </v-row>
+      <br />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+//export default {
+//async asyncData({ $axios }) {
+//const items = await $axios.$get("http://localhost:5000/");
+//return { items };
+//},
+//};
+</script>
