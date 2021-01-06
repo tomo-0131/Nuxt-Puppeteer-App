6f8d15f5dbb4081eacc8ca455d597ee89bcf8796
ecru <woovoodoor@yahoo.co.jp>
2021-01-07 00:01:29 +0900

    画像リンクを外す・パンくずリスト追加

--- pages/Mfpen.vue
+++ pages/Mfpen.vue
@@ -2,29 +2,30 @@
   <v-container>
     <v-row justify="center">
       <v-col cols="10">
-        <br />
+        <v-row justify="center">
+          <v-breadcrumbs :items="items" divider="/"></v-breadcrumbs>
+        </v-row>
         <v-col cols="12" lg="12" xs="4" class="pa-2">
-          <nuxt-link to="/mfpen">
-            <v-row justify="center">
-              <v-img
-                class="justify-center"
-                lazy-src="https://picsum.photos/id/11/10/6"
-                max-height="181"
-                max-width="202"
-                v-bind:src="require('@/assets/image/mfpen.png')"
-              ></v-img>
-            </v-row>
-          </nuxt-link>
+          <v-row justify="center">
+            <v-img
+              class="justify-center"
+              lazy-src="https://picsum.photos/id/11/10/6"
+              max-height="181"
+              max-width="202"
+              v-bind:src="require('@/assets/image/mfpen.png')"
+            ></v-img>
+          </v-row>
         </v-col>
         <br />
         <v-divider></v-divider>
         <br />
         <div style="white-space: wrap; word-wrap: break-word">
           <h5 class="grey--text text--darken-2 text-center">
-            2016年の秋冬よりスタートしたコペンハーゲンのメンズウェアブランド。
-            デザイナーはSigurdBank。様々なブランドでデザインやプロダクションを経験したのち独立。
-            スカンジナビアの伝統的な紳士服を、実験的で遊び心のあるデザインでモダンに仕上げ、リラックスしたムードが特徴。
-            生地とカッティングに拘りのあるブランドです。
+            <div class="text-center">
+              2016年の秋冬よりスタートしたコペンハーゲンのメンズウェアブランド。
+              <br />デザイナーはSigurdBank。<br />様々なブランドでデザインやプロダクションを<br />経験したのち独立。
+              <br />スカンジナビアの伝統的な紳士服を、実験的で遊び心のあるデザインでモダンに仕上げ、<br />リラックスしたムードが特徴。
+            </div>
           </h5>
         </div>
         <br />
@@ -45,4 +46,23 @@
 //return { items };
 //},
 //};
+</script>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      {
+        text: "home",
+        disabled: false,
+        href: "/",
+      },
+      {
+        text: "mfpen",
+        disabled: false,
+        href: "/mfpen",
+      },
+    ],
+  }),
+};
 </script>
