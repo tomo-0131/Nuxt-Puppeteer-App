47d39870191fd2089b9f786e02bf49a187ddab03
ecru <woovoodoor@yahoo.co.jp>
2021-01-06 23:11:48 +0900

    レイアウト修正

--- pages/Sacai.vue
+++ pages/Sacai.vue
@@ -1,40 +1,40 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="10">
-      <v-img
-        src="https://res.fashionsnap.com/image/upload/f_auto,q_auto/asset/collection/images/2020/08/sacai-21ss-pre-001.jpg?1607036664240"
-        height="210px"
-      ></v-img>
-
-      <br />
-      <div class="text-center">
-        <h3>S A C A I</h3>
-      </div>
-
-      <br />
-
-      <v-divider></v-divider>
-
-      <br />
-      <div style="white-space: pre-wrap; word-wrap: break-word">
-        <h5 class="grey--text text--darken-2 text-center">
-          Comme des Garçonsで勤めた経歴を持つ阿部千登勢が
-          1999年に立ち上げたブランド。
-          異素材をドッキングさせたデザインがアイコニックである。
-        </h5>
-      </div>
-
-      <br />
-
-      <v-divider></v-divider>
-
-      <br />
-
-      <ImageList />
-
-      <br />
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="10">
+        <br />
+        <v-col cols="12" lg="12" xs="4" class="pa-2">
+          <nuxt-link to="/sacai">
+            <v-row justify="center">
+              <v-img
+                class="justify-center"
+                lazy-src="https://picsum.photos/id/11/10/6"
+                max-height="181"
+                max-width="202"
+                v-bind:src="require('@/assets/image/sacai.png')"
+              ></v-img>
+            </v-row>
+          </nuxt-link>
+        </v-col>
+        <br />
+        <v-divider></v-divider>
+        <br />
+        <div style="white-space: wrap; word-wrap: break-word">
+          <h5 class="grey--text text--darken-2 text-center">
+            Comme des Garçonsで勤めた経歴を持つ阿部千登勢が
+            1999年に立ち上げたブランド。
+            異素材をドッキングさせたデザインがアイコニックである。
+          </h5>
+        </div>
+        <br />
+        <v-divider></v-divider>
+        <br />
+        <ImageList />
+        <br />
+        <br />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
