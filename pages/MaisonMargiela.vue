318812ed9b101aab0a6290695a4c1d1e160179d1
ecru <woovoodoor@yahoo.co.jp>
2021-01-06 23:11:42 +0900

    レイアウト修正

---
+++ pages/MaisonMargiela.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="10">
+        <br />
+        <v-col cols="12" lg="12" xs="4" class="pa-2">
+          <nuxt-link to="/maisonmargiela">
+            <v-row justify="center">
+              <v-img
+                class="justify-center"
+                lazy-src="https://picsum.photos/id/11/10/6"
+                max-height="181"
+                max-width="202"
+                v-bind:src="require('@/assets/image/maisonmargiela.jpeg')"
+              ></v-img>
+            </v-row>
+          </nuxt-link>
+        </v-col>
+        <br />
+        <v-divider></v-divider>
+        <br />
+        <div style="white-space: wrap; word-wrap: break-word">
+          <h5 class="grey--text text--darken-2 text-center">
+            2014年にMaison
+            Margiela（メゾン・マルジェラ）のクリエイティブディレクターに就任したJohn
+            Galliano（ジョン・ガリアーノ）は、ブランドを象徴する脱構築的デザインを踏襲し、
+            クラシックなシルエットを蘇らせました。非の打ち所のないハイクオリティなテーラリング技術が
+            堪能できるトレンチコートやシャツのほか、アイコニックな《Tabi（タビ）》シリーズのブーツ、
+            スニーカーなども必見です。
+          </h5>
+        </div>
+        <br />
+        <v-divider></v-divider>
+        <br />
+        <ImageList />
+        <br />
+        <br />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+//export default {
+//async asyncData({ $axios }) {
+//const items = await $axios.$get("http://localhost:5000/");
+//return { items };
+//},
+//};
+</script>
