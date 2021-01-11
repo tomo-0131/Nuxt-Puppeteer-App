62e7b9f22184aadcafc7870053a358f7d6667a3a
ecru <woovoodoor@yahoo.co.jp>
2021-01-11 23:59:58 +0900

    リファクタリング

--- pages/Mfpen.vue
+++ pages/Mfpen.vue
@@ -39,15 +39,6 @@
 </template>
 
 <script>
-//export default {
-//async asyncData({ $axios }) {
-//const items = await $axios.$get("http://localhost:5000/");
-//return { items };
-//},
-//};
-</script>
-
-<script>
 export default {
   data: () => ({
     items: [
