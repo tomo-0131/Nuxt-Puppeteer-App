619a613dfc18a3aacd4faf2c53bcb1a24cdac58d
ecru <woovoodoor@yahoo.co.jp>
2021-01-11 19:39:42 +0900

    フロント画面修正

--- pages/Sacai.vue
+++ pages/Sacai.vue
@@ -2,9 +2,6 @@
   <v-container>
     <v-row justify="center">
       <v-col cols="10">
-        <v-row justify="center">
-          <v-breadcrumbs :items="items" divider="/"></v-breadcrumbs>
-        </v-row>
         <v-col cols="12" lg="12" xs="4" class="pa-2">
           <v-row justify="center">
             <v-img
@@ -28,8 +25,36 @@
         <br />
         <v-divider></v-divider>
         <br />
-        <ImageList />
-        <br />
+        <div class="container">
+          <a
+            href="https://www.farfetch.com/jp/shopping/men/sacai--item-15467038.aspx?storeid=10167"
+          >
+            <v-img
+              class="justify-center"
+              max-height="181"
+              max-width="202"
+              v-bind:src="items.imageUrl"
+            ></v-img>
+          </a>
+          <br />
+          <tr v-for="item in items" :key="item.id">
+            <td>{{ item.itemName }}</td>
+            <br />
+            <td>{{ item.price }}</td>
+          </tr>
+        </div>
+
+        <v-btn
+          class="ma-2"
+          color="light-green darken-1"
+          elevation="5"
+          dark
+          nuxt
+          to="/"
+        >
+          <v-icon dark left> mdi-arrow-left </v-icon>
+          BACK
+        </v-btn>
         <br />
       </v-col>
     </v-row>
@@ -46,11 +71,22 @@ export default {
         href: "/",
       },
       {
-        text: "sacai",
+        text: "studio nicholson",
         disabled: false,
-        href: "/sacai",
+        href: "/studionicholson",
       },
     ],
   }),
+};
+</script>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const items = await $axios.$get("http://localhost:5000/api");
+    return {
+      items,
+    };
+  },
 };
 </script>
