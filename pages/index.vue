dc3a71e253d7a8252eb962ebeab274edd3450fe2
ecru <woovoodoor@yahoo.co.jp>
2021-01-11 20:33:16 +0900

    不要コード削除

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,3 @@
 <template>
   <Top />
 </template>
-
-<script>
-//export default {
-//async asyncData({ $axios }) {
-//const items = await $axios.$get("http://localhost:5000/");
-//return { items };
-//},
-//};
-</script>
