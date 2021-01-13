75ff16f6707713252de99903316775e380caaac3
ecru <woovoodoor@yahoo.co.jp>
2021-01-14 00:21:09 +0900

    ロゴのコンポーネント化

--- pages/MaisonMargiela.vue
+++ pages/MaisonMargiela.vue
@@ -5,16 +5,7 @@
         <v-row justify="center">
           <v-breadcrumbs :items="items" divider="/"></v-breadcrumbs>
         </v-row>
-        <v-col cols="12" lg="12" xs="4" class="pa-2">
-          <v-row justify="center">
-            <v-img
-              class="justify-center"
-              max-height="181"
-              max-width="202"
-              v-bind:src="require('@/assets/image/maisonmargiela.jpeg')"
-            ></v-img>
-          </v-row>
-        </v-col>
+        <MargielaLogo />
         <br />
         <v-divider></v-divider>
         <br />
