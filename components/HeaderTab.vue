d541ab7bd42164c0ab82cb2dcde0f80dfeeaf288
ecru <woovoodoor@yahoo.co.jp>
2021-01-14 00:03:51 +0900

    コンポーネント化(HeaderTab)

---
+++ components/HeaderTab.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-tool-bar dense fixed>
+    <br />
+    <v-toolbar-title class="text-center">
+      <nuxt-link to="/">P L I M E</nuxt-link>
+    </v-toolbar-title>
+    <br />
+
+    <v-tabs color="green" centered v-model="tab">
+      <v-tab
+        v-for="(menuItem, index) in menuItems"
+        :key="index"
+        :to="menuItem.url"
+      >
+        {{ menuItem.name }}
+      </v-tab>
+    </v-tabs>
+
+    <v-navigation-drawer v-model="drawer" temporary fixed>
+      <v-list nav dense>
+        <v-list-item-group>
+          <v-list-item
+            color="green"
+            v-for="(menuItem, index) in menuItems"
+            :key="index"
+            :to="menuItem.url"
+          >
+            <v-list-item-title>{{ menuItem.name }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </v-tool-bar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      menuItems: [
+        {
+          name: "HOME",
+          url: "/",
+        },
+        {
+          name: "ABOUT",
+          url: "/about",
+        },
+        {
+          name: "SERVICE",
+          url: "/service",
+        },
+      ],
+    };
+  },
+};
+</script>
