037db7f6b64758b138cc73cee545844013d75608
ecru <woovoodoor@yahoo.co.jp>
2021-01-14 00:21:01 +0900

    ロゴのコンポーネント化

--- pages/Mfpen.vue
+++ pages/Mfpen.vue
@@ -5,16 +5,7 @@
         <v-row justify="center">
           <v-breadcrumbs :items="items" divider="/"></v-breadcrumbs>
         </v-row>
-        <v-col cols="12" lg="12" xs="4" class="pa-2">
-          <v-row justify="center">
-            <v-img
-              class="justify-center"
-              max-height="181"
-              max-width="202"
-              v-bind:src="require('@/assets/image/mfpen.png')"
-            ></v-img>
-          </v-row>
-        </v-col>
+        <MfpenLogo />
         <br />
         <v-divider></v-divider>
         <br />
