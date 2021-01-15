4de339f7d034b94946f0398d59e99726b59fc244
ecru <woovoodoor@yahoo.co.jp>
2021-01-15 19:54:02 +0900

    修正

--- pages/Signin.vue
+++ pages/Signin.vue
@@ -1,45 +1,28 @@
 <template>
-  <v-card class="pa-md-9 mx-lg-auto" width="500px">
-    <v-form ref="form" v-model="valid" lazy-validation>
-      <v-text-field
-        prepend-icon="mdi-email"
-        v-model="email"
-        :rules="emailRules"
-        label="E-MAIL"
-        required
-      ></v-text-field>
-
-      <v-text-field
-        v-bind:type="showPassword ? 'text' : 'password'"
-        prepend-icon="mdi-lock"
-        v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-        label="パスワード"
-        @click:append="showPassword = !showPassword"
-        v-model="password"
-        :rules="passwordRules"
-        required
-      ></v-text-field>
-
-      <v-checkbox
-        v-model="checkbox"
-        :rules="[(v) => !!v || 'You must agree to continue!']"
-        label="Do you agree?"
-        required
-      ></v-checkbox>
-
-      <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">
-        Validate
-      </v-btn>
-
-      <v-btn color="error" class="mr-4" @click="reset"> Reset Form </v-btn>
-    </v-form>
-  </v-card>
+  <div class="login-form">
+    <form @submit.prevent="login">
+      <p class="error" v-if="error">{{ error }}</p>
+      <p>
+        <input type="text" v-model="email" placeholder="email" name="email" />
+      </p>
+      <p>
+        <input
+          type="text"
+          v-model="password"
+          placeholder="password"
+          name="password"
+        />
+      </p>
+      <div class="login-btn">
+        <button type="submit">ログイン</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    showPassword: false;
     return {
       error: null,
       email: "",
@@ -47,43 +30,18 @@ export default {
     };
   },
   methods: {
-    async registration() {
+    async login() {
+      console.log(this.registeredUser);
       try {
         await this.$store.dispatch("login", {
           email: this.email,
           password: this.password,
+          authData: this.$store.state.registeredUser,
         });
         this.$router.push("/");
       } catch (e) {
         this.error = e.message;
       }
-    },
-  },
-};
-</script>
-
-<script>
-export default {
-  data: () => ({
-    valid: true,
-    name: "",
-    passwordRules: [
-      (v) => !!v || "PASSWORD is required",
-      (v) => (v && v.length <= 10) || "Name must be less than 10 characters",
-    ],
-    email: "",
-    emailRules: [
-      (v) => !!v || "E-MAILS is required",
-      (v) => /.+@.+\..+/.test(v) || "E-MAIL must be valid",
-    ],
-  }),
-
-  methods: {
-    validate() {
-      this.$refs.form.validate();
-    },
-    reset() {
-      this.$refs.form.reset();
     },
   },
 };
--- store/mutations.js
+++ store/mutations.js
@@ -1,5 +1,10 @@
 export default {
+  REGISTER_USER: function (state, data) {
+    state.registeredUser["email"] = data["email"]
+    state.registeredUser["password"] = data["password"]
+    state.authUser = data
+  },
   AUTHED_USER: function (state, data) {
-    state.authed_user = data
+    state.authUser = data // 入力したemailとpasswordがここに入る
   }
 }
