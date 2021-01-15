815adfbe70eadda816fd92b2f8dc9c088deaf5cc
ecru <woovoodoor@yahoo.co.jp>
2021-01-15 16:35:00 +0900

    mutationに渡すコード追記

--- store/action.js
+++ store/action.js
@@ -1,5 +1,14 @@
-const { Template } = require("webpack");
-
-<template>
-
-</template>
+export default {
+  async login({ commit },{ email, password}) {
+    console.log(commit)
+    try {
+      if( email != "test@email.com" || password != "12345678910") {
+        throw new Error("error!")
+      }
+      let data ={ email: email, password: password}
+      commit("AUTHED_USER", data)
+    } catch(e) {
+      throw e
+    }
+  }
+}
