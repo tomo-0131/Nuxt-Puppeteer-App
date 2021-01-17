759d609a62e5c753d614536bf57ea193b01d4ab4
ecru <woovoodoor@yahoo.co.jp>
2021-01-17 15:01:21 +0900

    ログアウトボタンをコンポーネント化

---
+++ components/LogoutButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-btn color="light-green" dark @click="logout()">ログアウト</v-btn>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      this.$store.commit("updateIdToken", null);
+      this.$router.push("/signup");
+    },
+  },
+};
+</script>
