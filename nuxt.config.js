1f46f4c4e1726773315d500063080ea2d7607464
tomo-0131 <woovoodoor@yahoo.co.jp>
2021-02-05 17:16:52 +0900

    title修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,8 +8,8 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - nuxt-scraping-app',
-    title: 'nuxt-scraping-app',
+    titleTemplate: '%s - tomoyuki-kumagai',
+    title: 'tomoyuki-kumagai',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
